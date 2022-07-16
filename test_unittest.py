e9eaf2ecc036ccf6532d82b8a5df0017c8329b89
Niels Humbeck <nielshumbeck91@googlemail.com>
2022-07-16 21:59:28 +0200

    AddUnitTests

---
+++ test_unittest.py
@@ -0,0 +1,11 @@
+import unittest
+from lambda_tweets_loader.getTimelineUtil import create_url
+
+
+class TestEmployee(unittest.TestCase):
+
+    def test_create_url(self):
+        print('test create_url')
+        self.assertEqual(create_url("NielsID","202202011000",max_results=100), 'https://api.twitter.com/2/users/NielsID/tweets?max_results=100&start_time=202202011000')
+        self.assertEqual(create_url("NielsID","202202011000",pagination_token="DiesIstEinPaginationToken",max_results=100), 'https://api.twitter.com/2/users/NielsID/tweets?max_results=100&start_time=202202011000&pagination_token=DiesIstEinPaginationToken')
+
